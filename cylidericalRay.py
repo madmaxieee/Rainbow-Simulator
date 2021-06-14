b62ac69315d8c14555a26de0b64363674c55ad18
zhuang-jia-xu <chuangjiaxu@gmail.com>
2021-06-15 00:54:35 +0800

    finalizing
    finalizing

--- cylidericalRay.py
+++ cylidericalRay.py
@@ -4,6 +4,17 @@ import numpy as np
 nair = 1.0
 nwater = [1.3320, 1.3325, 1.3331, 1.3349, 1.3390, 1.3435]
 
+scene = canvas(
+    background=vec(1, 1, 1),
+    width=1200,
+    height=600,
+    center=vec(0, 0, 0),
+    range=10,
+    userspin=False,
+    userpan=False,
+    userzoom=True,
+    caption='droplet',
+)
 
 class Ray:
     def __init__(self, _pos=vec(0, 0, 0), angle=0):
@@ -14,7 +25,7 @@ class Ray:
         self.axis = []
         self.log = [[], [], [], [], [], []]
         self.opacity = [[], [], [], [], [], []]
-        self.exit_angles=[]
+        self.exit_angles = []
         self.dt = 0.001
 
         self.in_lens = [False, False, False, False, False, False]
@@ -27,64 +38,93 @@ class Ray:
             self.pos = np.append(self.pos, _pos)
 
         beam_radius = 0.05
-        self.white = cylinder(
-            pos=_pos,
-            color=color.white,
-            radius=beam_radius,
-            axis=vec(0, 0, 0),
-            ballv=vec(cos(-angle), sin(-angle), 0),
-            ballpos=_pos
-        )
-        self.beams = [
-            [cylinder(pos=_pos, color=color.red,
-                      radius=beam_radius, visible=False)],
-            [cylinder(pos=_pos, color=color.orange,
-                      radius=beam_radius, visible=False)],
-            [cylinder(pos=_pos, color=color.yellow,
-                      radius=beam_radius, visible=False)],
-            [cylinder(pos=_pos, color=color.green,
-                      radius=beam_radius, visible=False)],
-            [cylinder(pos=_pos, color=color.blue,
-                      radius=beam_radius, visible=False)],
-            [cylinder(pos=_pos, color=color.purple,
-                      radius=beam_radius, visible=False)]
-        ]
+        self.white = cylinder(pos=_pos,
+                              color=color.white,
+                              radius=beam_radius,
+                              axis=vec(0, 0, 0),
+                              ballv=vec(cos(-angle), sin(-angle), 0),
+                              ballpos=_pos,
+                              canvas=scene)
+        self.beams = [[
+            cylinder(pos=_pos,
+                     color=color.red,
+                     radius=beam_radius,
+                     visible=False,
+                     canvas=scene)
+        ],
+                      [
+                          cylinder(pos=_pos,
+                                   color=color.orange,
+                                   radius=beam_radius,
+                                   visible=False,
+                                   canvas=scene)
+                      ],
+                      [
+                          cylinder(pos=_pos,
+                                   color=color.yellow,
+                                   radius=beam_radius,
+                                   visible=False,
+                                   canvas=scene)
+                      ],
+                      [
+                          cylinder(pos=_pos,
+                                   color=color.green,
+                                   radius=beam_radius,
+                                   visible=False,
+                                   canvas=scene)
+                      ],
+                      [
+                          cylinder(pos=_pos,
+                                   color=color.blue,
+                                   radius=beam_radius,
+                                   visible=False,
+                                   canvas=scene)
+                      ],
+                      [
+                          cylinder(pos=_pos,
+                                   color=color.purple,
+                                   radius=beam_radius,
+                                   visible=False,
+                                   canvas=scene)
+                      ]]
 
     # does refract if there is one
     # param type = 'in' or 'out'
     def refract(self, droplet_pos, droplet_r):
-        def Rs(i, t): return (sin(t-i)/sin(t+i))**2
-        def Rp(i, t): return (tan(t-i)/tan(t+i))**2
+        def Rs(i, t):
+            return (sin(t - i) / sin(t + i))**2
+
+        def Rp(i, t):
+            return (tan(t - i) / tan(t + i))**2
+
         for i in range(6):
-            if not self.in_lens[i] and self.reflected[i] == 0 and mag(droplet_pos - self.pos[i]) <= droplet_r:
+            if not self.in_lens[i] and self.reflected[i] == 0 and mag(
+                    droplet_pos - self.pos[i]) <= droplet_r:
                 type = 'in'
                 self.in_lens[i] = True
                 self.log[i].append(self.pos[i])
                 self.white.ballv = vec(0, 0, 0)
                 self.white.axis = self.white.ballpos - self.white.pos
-            elif self.in_lens[i] and self.reflected[i] >= 1 and mag(droplet_pos - self.pos[i]) >= droplet_r:
+            elif self.in_lens[i] and self.reflected[i] >= 1 and mag(
+                    droplet_pos - self.pos[i]) >= droplet_r:
                 type = 'out'
                 self.in_lens[i] = False
                 self.log[i].append(self.pos[i])
                 self.beams[i].append(
-                    cylinder(
-                        pos=self.log[i][1],
-                        color=self.beams[i][0].color,
-                        radius=self.beams[i][0].radius,
-                        axis=self.log[i][2] - self.log[i][1],
-                        opacity=self.opacity[i][1] * self.opacity[i][0]
-                    )
-                )
+                    cylinder(pos=self.log[i][1],
+                             color=self.beams[i][0].color,
+                             radius=self.beams[i][0].radius,
+                             axis=self.log[i][2] - self.log[i][1],
+                             opacity=self.opacity[i][1] * self.opacity[i][0],
+                             canvas=scene))
                 # print(
                 #     f'self.opacity[{i}][1] = {self.opacity[i][1] * self.opacity[i][0]}')
                 self.beams[i].append(
-                    cylinder(
-                        pos=self.log[i][2],
-                        color=self.beams[i][0].color,
-                        radius=self.beams[i][0].radius,
-                        axis=self.pos[i] - self.log[i][2]
-                    )
-                )
+                    cylinder(pos=self.log[i][2],
+                             color=self.beams[i][0].color,
+                             radius=self.beams[i][0].radius,
+                             axis=self.pos[i] - self.log[i][2],
+                             canvas=scene))
             else:
                 continue
             n1 = nair if type == 'in' else nwater[i]
@@ -95,13 +135,13 @@ class Ray:
             angle_out = asin(n1 / n2 * sin(angle_in))
             self.opacity[i].append(
                 1 - (Rs(angle_in, angle_out) + Rp(angle_in, angle_out)) / 2)
-            self.v[i] = rotate(
-                normal_v,
-                angle=angle_out,
-                axis=cross(normal_v, self.v[i])
-            )
-            if type == 'out' and (i == 0 or i == 5) and len(self.beams[i]) == 3:
-                self.exit_angles.append(diff_angle(self.v[i], -self.white.axis) * 180/pi)
+            self.v[i] = rotate(normal_v,
+                               angle=angle_out,
+                               axis=cross(normal_v, self.v[i]))
+            if type == 'out' and (i == 0 or i == 5) and len(
+                    self.beams[i]) == 3:
+                self.exit_angles.append(
+                    diff_angle(self.v[i], -self.white.axis) * 180 / pi)
                 # self.angle_labels.append(label(
                 #     pos=self.beams[i][2].pos + self.beams[i][2].axis / 2,
                 #     text="{0:4.2f}deg".format(diff_angle(
@@ -112,10 +152,15 @@ class Ray:
             self.pos[i] += self.v[i] * 0.1
 
     def reflect(self, droplet_pos, droplet_r):
-        def Rs(i, t): return (sin(t-i)/sin(t+i))**2
-        def Rp(i, t): return (tan(t-i)/tan(t+i))**2
+        def Rs(i, t):
+            return (sin(t - i) / sin(t + i))**2
+
+        def Rp(i, t):
+            return (tan(t - i) / tan(t + i))**2
+
         for i in range(6):
-            if self.in_lens[i] and self.reflected[i] == 0 and mag(droplet_pos - self.pos[i]) >= 0.999 * droplet_r:
+            if self.in_lens[i] and self.reflected[i] == 0 and mag(
+                    droplet_pos - self.pos[i]) >= 0.999 * droplet_r:
                 self.reflected[i] += 1
                 #  show the beam from refraction point to reflection point
                 self.log[i].append(self.pos[i])
@@ -132,7 +177,8 @@ class Ray:
                     (Rs(angle_in, angle_out) + Rp(angle_in, angle_out)) / 2)
 
                 angle_ref = diff_angle(-self.v[i], normal_v)
-                self.v[i] = rotate(normal_v, angle=angle_ref,
+                self.v[i] = rotate(normal_v,
+                                   angle=angle_ref,
                                    axis=cross(normal_v, self.v[i]))
                 self.pos[i] += self.v[i] * 0.1
                 # print(f'reflect {i}')
@@ -167,3 +213,16 @@ class Ray:
                 self.beams[i][0].visible = False
                 del self.beams[i][0]
 
+
+r = 5
+droplet = sphere(radius=r,
+                 pos=vec(0, 0, 0),
+                 color=vec(195 / 255, 253 / 255, 255 / 255),
+                 opacity=0.1,
+                 canvas=scene)
+dropletCenter = sphere(radius=0.05,
+                       pos=droplet.pos,
+                       color=color.red,
+                       canvas=scene)
+
+ray_arr = []
---
+++ rainbow.py
@@ -0,0 +1,142 @@
+from vpython import *
+
+scene2 = canvas(
+    background=vec(1, 1, 1),
+    width=1200,
+    height=600,
+    center=vec(0, 0, 0),
+    range=120,
+    forward=vec(1, -1, -2),
+    userspin=True,
+    userpan=False,
+    userzoom=True,
+    caption='rainbow',
+)
+
+earth = box(size=vec(1000, 1, 1000), color=color.green, opacity=0.8)
+
+observer = sphere(pos=vec(0, 0, 0), radius=5, color=color.blue)
+
+observer_label = label(pos=observer.pos,
+                       text='observer',
+                       xoffset=-30,
+                       yoffset=30,
+                       box=False)
+
+main_axis = cylinder(pos=observer.pos,
+                     color=color.black,
+                     radius=1,
+                     axis=vec(200, 0, 0))
+
+anti_axis = cylinder(pos=observer.pos,
+                     color=color.black,
+                     radius=1,
+                     axis=-main_axis.axis)
+
+_thickness = 1
+_rainbow = [
+    ring(pos=main_axis.axis / 2,
+         axis=main_axis.axis,
+         radius=mag(main_axis.axis / 2) * tan(42 * pi / 180),
+         thickness=_thickness,
+         color=color.red),
+    ring(pos=main_axis.axis / 2,
+         axis=main_axis.axis,
+         radius=mag(main_axis.axis / 2) * tan(41.8 * pi / 180),
+         thickness=_thickness,
+         color=color.orange),
+    ring(pos=main_axis.axis / 2,
+         axis=main_axis.axis,
+         radius=mag(main_axis.axis / 2) * tan(41.2 * pi / 180),
+         thickness=_thickness,
+         color=color.yellow),
+    ring(pos=main_axis.axis / 2,
+         axis=main_axis.axis,
+         radius=mag(main_axis.axis / 2) * tan(40.8 * pi / 180),
+         thickness=_thickness,
+         color=color.green),
+    ring(pos=main_axis.axis / 2,
+         axis=main_axis.axis,
+         radius=mag(main_axis.axis / 2) * tan(40.4 * pi / 180),
+         thickness=_thickness,
+         color=color.blue),
+    ring(pos=main_axis.axis / 2,
+         axis=main_axis.axis,
+         radius=mag(main_axis.axis / 2) * tan(40 * pi / 180),
+         thickness=_thickness,
+         color=color.purple)
+]
+
+sun_light = [
+    cylinder(pos=_rainbow[0].pos +
+             _rainbow[0].radius * rotate(norm(main_axis.axis), pi / 2),
+             radius=0.5,
+             axis=-main_axis.axis,
+             color=color.white),
+    cylinder(pos=_rainbow[5].pos +
+             _rainbow[5].radius * rotate(norm(main_axis.axis), pi / 2),
+             radius=0.5,
+             axis=-main_axis.axis,
+             color=color.white)
+]
+
+rays = [
+    arrow(pos=sun_light[0].pos,
+          shaftwidth=0.5,
+          headwidth=2,
+          axis=1 / cos(42 * pi / 180) *
+          rotate(-main_axis.axis / 2, 42 * pi / 180),
+          color=color.red),
+    arrow(pos=sun_light[0].pos,
+          shaftwidth=0.5,
+          headwidth=2,
+          axis=1 / cos(40 * pi / 180) *
+          rotate(-main_axis.axis / 2, 40 * pi / 180),
+          color=color.purple),
+    arrow(pos=sun_light[1].pos,
+          shaftwidth=0.5,
+          headwidth=2,
+          axis=1 / cos(42 * pi / 180) *
+          rotate(-main_axis.axis / 2, 42 * pi / 180),
+          color=color.red),
+    arrow(pos=sun_light[1].pos,
+          shaftwidth=0.5,
+          headwidth=2,
+          axis=1 / cos(40 * pi / 180) *
+          rotate(-main_axis.axis / 2, 40 * pi / 180),
+          color=color.purple)
+]
+
+
+def rotate_rainbow(slider):
+    global main_axis, anti_axis, _rainbow, sun_light, rays
+
+    angle = -slider.value
+    main_axis.axis = rotate(vec(200, 0, 0), angle)
+    anti_axis.axis = -main_axis.axis
+
+    for i in range(6):
+        _rainbow[i].visible = False
+        _rainbow[i].axis = main_axis.axis
+        _rainbow[i].pos = main_axis.axis / 2
+        _rainbow[i].visible = True
+
+    sun_light[0].axis = -main_axis.axis
+    sun_light[0].pos = _rainbow[0].pos + _rainbow[0].radius * rotate(
+        norm(main_axis.axis), pi / 2)
+    sun_light[1].axis = -main_axis.axis
+    sun_light[1].pos = _rainbow[5].pos + _rainbow[5].radius * rotate(
+        norm(main_axis.axis), pi / 2)
+
+    rays[0].pos = sun_light[0].pos
+    rays[0].axis = 1 / cos(42 * pi / 180) * rotate(-main_axis.axis / 2,
+                                                   42 * pi / 180)
+    rays[1].pos = sun_light[0].pos
+    rays[1].axis = 1 / cos(40 * pi / 180) * rotate(-main_axis.axis / 2,
+                                                   40 * pi / 180)
+    rays[2].pos = sun_light[1].pos
+    rays[2].axis = 1 / cos(42 * pi / 180) * rotate(-main_axis.axis / 2,
+                                                   42 * pi / 180)
+    rays[3].pos = sun_light[1].pos
+    rays[3].axis = 1 / cos(40 * pi / 180) * rotate(-main_axis.axis / 2,
+                                                   40 * pi / 180)
